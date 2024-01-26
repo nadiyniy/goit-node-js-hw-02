ec7766ccadd357edd413aab7a66b445e0b6b6a6f
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2024-01-26 20:18:43 +0200

    finish HW-5

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require('express')
 
-const authController = require('../../controllers/auth');
-const { usersSchemas } = require('../../validators');
-const { validateBody, authenticate } = require('../../middlewares');
+const authController = require('../../controllers/auth')
+const { usersSchemas } = require('../../validators')
+const { validateBody, authenticate, upload } = require('../../middlewares')
 
-const router = express.Router();
+const router = express.Router()
 
-router.post('/register', validateBody(usersSchemas.registerUser), authController.register);
-router.post('/login', validateBody(usersSchemas.loginUser), authController.login);
-router.get('/current', authenticate, authController.current);
-router.get('/logout', authenticate, authController.logout);
+router.post('/register', validateBody(usersSchemas.registerUser), authController.register)
+router.post('/login', validateBody(usersSchemas.loginUser), authController.login)
+router.get('/current', authenticate, authController.current)
+router.get('/logout', authenticate, authController.logout)
+router.patch('/avatars', upload.single('avatar'), authenticate, authController.updateAvatar)
 
-module.exports = router;
+module.exports = router
